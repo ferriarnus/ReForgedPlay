636fc2d89954bbfd10b0c6a94381183620ae615e
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2024-05-11 18:39:02 +0200

    Update Mixin_RenderHudCallback.java

--- src/main/java/de/johni0702/minecraft/gui/versions/mixin/Mixin_RenderHudCallback.java
+++ src/main/java/de/johni0702/minecraft/gui/versions/mixin/Mixin_RenderHudCallback.java
@@ -4,6 +4,8 @@ package de.johni0702.minecraft.gui.versions.mixin;
 import de.johni0702.minecraft.gui.versions.callbacks.RenderHudCallback;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.neoforged.neoforge.client.gui.overlay.ExtendedGui;
+import net.neoforged.neoforge.client.gui.overlay.VanillaGuiOverlay;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,10 +14,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 //$$ import net.minecraft.client.util.math.MatrixStack;
 //#endif
 
-@Mixin(InGameHud.class)
+@Mixin(VanillaGuiOverlay.class)
 public class Mixin_RenderHudCallback {
     @Inject(
-            method = "render",
+            method = "lambda$static$19",
             //#if MC>=12002
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/DebugHud;shouldShowDebugHud()Z")
             //#else
@@ -23,7 +25,7 @@ public class Mixin_RenderHudCallback {
             //#endif
     )
     //#if MC>=12000
-    private void renderOverlay(DrawContext stack, float partialTicks, CallbackInfo ci) {
+    private static void renderOverlay(ExtendedGui gui, DrawContext stack, float partialTicks, int screenWidth, int screenHeight, CallbackInfo ci) {
     //#elseif MC>=11600
     //$$ private void renderOverlay(MatrixStack stack, float partialTicks, CallbackInfo ci) {
     //#else
